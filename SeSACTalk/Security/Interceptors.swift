a3640a37908a142fc01d39786fdd7da94e960e7d
Louie Shin <unicus.fortis@gmail.com>
2024-01-24 04:50:48 +0900

    [Fix] Interceptor 에 Dispatch Group 적용
    비동기 작업이 완료되기 전 retry 가 실행되는 문제 해결

--- SeSACTalk/Security/Interceptors.swift
+++ SeSACTalk/Security/Interceptors.swift
@@ -19,7 +19,7 @@ final class RefreshTokenInterceptor: RequestInterceptor {
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         
         var urlRequest = urlRequest
-
+        
         urlRequest.headers.update(name: SecureKeys.Headers.auth, value: SecureKeys.Headers.accessToken)
         
         completion(.success(urlRequest))
@@ -28,6 +28,8 @@ final class RefreshTokenInterceptor: RequestInterceptor {
     
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         print("========리트라이 시작")
+        let dispatchGroup = DispatchGroup()
+        
         if let response = request as? DataRequest {
             guard let errorData = response.data,
                   let decoded = try? JSONDecoder().decode(ErrorResponse.self, from: errorData)
@@ -40,26 +42,35 @@ final class RefreshTokenInterceptor: RequestInterceptor {
                     
                     retryCount += 1
                     
+                    dispatchGroup.enter()
                     let token = AccessTokenRequest(accessToken: SecureKeys.createAccessToken())
                     networkmanager.fetchRequest(endpoint: .refreshToken(token: token),
                                                 decodeModel: AccessTokenRequest.self)
-                        .subscribe(with: self) { owner, result in
-                            switch result {
-                            case .success(let response) :
-                                SecureKeys.saveNewAccessToken(token: response.accessToken)
-                                print("토큰저장완료")
-                            case .failure(let error) :
-                                print(error.errorCode)
-                                completion(.doNotRetry)
-                            }
+                    .subscribe(with: self) { owner, result in
+                        switch result {
+                        case .success(let response) :
+                            SecureKeys.saveNewAccessToken(token: response.accessToken)
+                            print("토큰저장완료")
+                            completion(.retry)
+                            dispatchGroup.leave()
+                            
+                        case .failure(let error) :
+                            print(error.errorCode)
+                            completion(.doNotRetry)
+                            dispatchGroup.leave()
+                            
                         }
-                        .disposed(by: disposeBag)
+                    }
+                    .disposed(by: disposeBag)
                 } else {
                     completion(.doNotRetry)
                 }
                 
             default: break
             }
+        }
+        dispatchGroup.notify(queue: .main) {
+            completion(.retry)
         }
     }
 }
