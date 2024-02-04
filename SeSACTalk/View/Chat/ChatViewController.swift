f652cdae1b82e63752dce00941d7ccc706f09ab8
Louie Shin <unicus.fortis@gmail.com>
2024-02-05 05:22:11 +0900

    [Add] ChatViewController 에 InputAccessoryView 추가
    Textview 에 입력된 라인 수에 따라 높이가 유동적으로 조정되는 제약 사항 추가
    테이블뷰를 드래그 했을때 키보드가 dismiss 되는 기능 추가

--- SeSACTalk/View/Chat/ChatViewController.swift
+++ SeSACTalk/View/Chat/ChatViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 import SnapKit
 import RxSwift
 import RxCocoa
-import MessageKit
-import InputBarAccessoryView
+import RxKeyboard
 
 
 final class ChatViewController: UIViewController {
@@ -18,8 +17,9 @@ final class ChatViewController: UIViewController {
     private var manager = ChatManager()
     private let session = LoginSession.shared
     private let tableView = UITableView(frame: .zero,
-                                        style: .grouped)
+                                        style: .plain)
     private let disposeBag = DisposeBag()
+    private let accView = CustomInputAccView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,19 +35,29 @@ final class ChatViewController: UIViewController {
         navigationController?.setChannelChatNavigation(target: self,
                                                        rightAction: #selector(setNavigationRightAction))
         view.addSubview(tableView)
-        tableView.register(MessageTableCell.self,
-                           forCellReuseIdentifier: MessageTableCell.identifier)
+        view.addSubview(accView)
+        
+        tableView.register(MessageSenderTableCell.self,
+                           forCellReuseIdentifier: MessageSenderTableCell.identifier)
         tableView.backgroundColor = .clear
         tableView.estimatedRowHeight = 50
         tableView.rowHeight = UITableView.automaticDimension
         tableView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
         tableView.separatorStyle = .none
         tableView.contentInsetAdjustmentBehavior = .never
+        tableView.keyboardDismissMode = .onDrag
+        accView.textView.delegate = self
     }
     
     private func setConstraints() {
         tableView.snp.makeConstraints { make in
             make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        accView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(40)
             make.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
@@ -59,19 +69,47 @@ final class ChatViewController: UIViewController {
     }
     
     private func bind() {
-        print("바인드")
         manager.chatDatasRelay
             .observe(on: MainScheduler.instance)
-            .bind(to: tableView.rx.items(cellIdentifier: MessageTableCell.identifier,
-                                         cellType: MessageTableCell.self)) {
+            .bind(to: tableView.rx.items(cellIdentifier: MessageSenderTableCell.identifier,
+                                         cellType: MessageSenderTableCell.self)) {
                 _, item, cell in
                 
                 cell.datas.accept(item)
                 
             }.disposed(by: disposeBag)
+        
+        accView.textView.rx.observe(CGSize.self, "contentSize")
+            .observe(on: MainScheduler.instance)
+            .subscribe(with: self) { owner, size in
+                guard let height = size?.height else { return }
+                if owner.accView.textView.numberOfLines < 3 {
+                    owner.accView.snp.updateConstraints { make in
+                        make.height.equalTo(height + 8)
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
     @objc private func setNavigationRightAction() {
         print("메롱")
     }
 }
+
+extension ChatViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.textColor = .black
+        textView.text = nil
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "메세지를 입력하세요"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+}
+
+
