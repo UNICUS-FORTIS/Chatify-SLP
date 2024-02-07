07954b566c3b859e1cedda8cb3b3ed4b6a397105
Louie Shin <unicus.fortis@gmail.com>
2024-02-07 23:51:12 +0900

    [Feat] ChatViewContoller 에 셀 데이터 바인딩 +
    셀 데이터 바인딩과 메세지 전송 트리거 추가 외

--- SeSACTalk/View/Chat/ChatViewController.swift
+++ SeSACTalk/View/Chat/ChatViewController.swift
@@ -14,12 +14,14 @@ import RxKeyboard
 
 final class ChatViewController: UIViewController {
     
-    private var manager = ChatManager()
+    private var manager: ChatManager
     private let session = LoginSession.shared
+    private lazy var sender = session.makeUserID()
     private let tableView = UITableView(frame: .zero,
                                         style: .plain)
     private let disposeBag = DisposeBag()
     private let accView = CustomInputAccView()
+    
     
     private var lastIndexPath: IndexPath? {
         guard let lastRow = tableView.numberOfRows(inSection: 0) > 1 ?
@@ -28,14 +30,29 @@ final class ChatViewController: UIViewController {
         return IndexPath(row: lastRow, section: 0)
     }
     
+    init(manager: ChatManager) {
+        self.manager = manager
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
         setConstraints()
         bind()
         setUp()
-        manager.mockUP()
         keyboardSetting()
+        setUpSendButton()
+        manager.socketManager.startConnect(channelID: manager.createChannelID())
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        manager.socketManager.closeWebSocket()
     }
     
     private func configure() {
@@ -44,17 +61,21 @@ final class ChatViewController: UIViewController {
                                                        rightAction: #selector(setNavigationRightAction))
         view.addSubview(tableView)
         view.addSubview(accView)
+        tableView.register(MessageTableCell.self,
+                           forCellReuseIdentifier: MessageTableCell.identifier)
         
         tableView.register(MessageSenderTableCell.self,
                            forCellReuseIdentifier: MessageSenderTableCell.identifier)
         tableView.backgroundColor = .clear
         tableView.estimatedRowHeight = 50
         tableView.rowHeight = UITableView.automaticDimension
-        tableView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
         tableView.separatorStyle = .none
         tableView.contentInsetAdjustmentBehavior = .never
         tableView.keyboardDismissMode = .onDrag
         accView.textView.delegate = self
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tableView.addGestureRecognizer(tapGesture)
     }
     
     private func setConstraints() {
@@ -73,19 +94,40 @@ final class ChatViewController: UIViewController {
     private func setUp() {
         manager.loadChatLog()
         manager.fetchUnreadDatas()
-        manager.startSocketConnect()
     }
     
     private func bind() {
+//        manager.chatDatasRelay
+//            .observe(on: MainScheduler.instance)
+//            .bind(to: tableView.rx.items(cellIdentifier: MessageSenderTableCell.identifier,
+//                                         cellType: MessageSenderTableCell.self)) {
+//                _, item, cell in
+//                
+//                cell.datas.accept(item)
+//                
+//            }.disposed(by: disposeBag)
+        
         manager.chatDatasRelay
             .observe(on: MainScheduler.instance)
-            .bind(to: tableView.rx.items(cellIdentifier: MessageSenderTableCell.identifier,
-                                         cellType: MessageSenderTableCell.self)) {
-                _, item, cell in
+            .bind(to: tableView.rx.items) { [weak self] (tableView, row, item) -> UITableViewCell in
+                guard let strongSelf = self else { return UITableViewCell() }
                 
-                cell.datas.accept(item)
-                
-            }.disposed(by: disposeBag)
+                if strongSelf.manager.judgeSender(sender: strongSelf.sender, userID: item.user.userID) {
+                    
+                    guard let senderCell = tableView.dequeueReusableCell(withIdentifier: MessageSenderTableCell.identifier) as? MessageSenderTableCell else { return UITableViewCell() }
+                    senderCell.datas.accept(item)
+                    
+                    return senderCell
+                    
+                } else {
+                    
+                    guard let otherUserCell = tableView.dequeueReusableCell(withIdentifier: MessageTableCell.identifier) as? MessageTableCell else { return UITableViewCell() }
+                    
+                    otherUserCell.datas.accept(item)
+                    return otherUserCell
+                }
+            }
+            .disposed(by: disposeBag)
         
         accView.textView.rx.observe(CGSize.self, "contentSize")
             .observe(on: MainScheduler.instance)
@@ -102,12 +144,6 @@ final class ChatViewController: UIViewController {
     
     private func keyboardSetting() {
         
-        RxKeyboard.instance.frame
-            .drive(onNext: { frame in
-                print(frame)
-            })
-            .disposed(by: disposeBag)
-        
         RxKeyboard.instance.visibleHeight
             .drive(with: self) { owner, height  in
                 owner.accView.snp.updateConstraints { make in
@@ -123,6 +159,25 @@ final class ChatViewController: UIViewController {
     
     @objc private func setNavigationRightAction() {
         print("메롱")
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func setUpSendButton() {
+        accView.rightButton.rx.tap
+            .bind(with: self) { owner, _ in
+                let message = owner.accView.textView.text
+                let files: [Data] = []
+                let request = ChatBodyRequest(content: message, files: files)
+                owner.manager.messageSender(request: request)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    deinit {
+        print("채팅 뷰컨트롤러 Deinit")
     }
 }
 
