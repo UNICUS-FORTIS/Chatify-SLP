3b1c7c2a828204abf812c625c0fd2e34a50cffd9
Louie Shin <unicus.fortis@gmail.com>
2024-02-12 20:33:27 +0900

    [Feat] DefaultWorkSpaceViewController 의 프로필 아이콘 터치시 동작 추가

--- SeSACTalk/View/WorkSpace/Default/DefaultWorkSpaceViewController.swift
+++ SeSACTalk/View/WorkSpace/Default/DefaultWorkSpaceViewController.swift
@@ -120,11 +120,18 @@ final class DefaultWorkSpaceViewController: UIViewController {
         tableView.register(ChannelFooterCell.self,
                            forHeaderFooterViewReuseIdentifier: ChannelFooterCell.identifier)
         
-        navigationController?.setWorkSpaceNavigation(target: self, action: #selector(workSpaceTitleTapped))
+        navigationController?.setWorkSpaceNavigation(target: self,
+                                                     leftAction: #selector(workSpaceTitleTapped),
+                                                     rightActoin: #selector(profileImageTapped))
     }
     
     @objc func workSpaceTitleTapped() {
         present(SideMenuManager.default.leftMenuNavigationController!, animated: true, completion: nil)
+    }
+    
+    @objc func profileImageTapped() {
+        let vc = ProfileViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     
