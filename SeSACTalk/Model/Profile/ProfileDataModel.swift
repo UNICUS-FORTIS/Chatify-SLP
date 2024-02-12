c6b52e6e65c57d173fd8da3972fb4bfa1d4d2cf5
Louie Shin <unicus.fortis@gmail.com>
2024-02-12 20:03:11 +0900

    [Add] ProfileDataModel 생성
    프로필 데이터를 셀에 보여줄수있는 구조체 생성

---
+++ SeSACTalk/Model/Profile/ProfileDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileDataModel.swift
+//  SeSACTalk
+//
+//  Created by LOUIE MAC on 2/11/24.
+//
+
+import Foundation
+
+
+struct ProfileDataModel {
+    
+    let title: String
+    let value: String
+    
+}
