14502381e42b24a9918cd872368677d95b694293
Louie Shin <unicus.fortis@gmail.com>
2024-02-13 17:39:47 +0900

    [Add] ProfileUpdateRequest 네트워크 Encodable 구조체 생성

---
+++ SeSACTalk/Model/Request/ProfileUpdateRequest.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileUpdateRequest.swift
+//  SeSACTalk
+//
+//  Created by LOUIE MAC on 2/13/24.
+//
+
+import Foundation
+
+
+struct ProfileUpdateRequest: Encodable {
+    
+    let nickname: String
+    let phone: String?
+    
+}
