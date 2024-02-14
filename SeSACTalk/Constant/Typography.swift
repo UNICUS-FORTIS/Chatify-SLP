d0c2876e618848b31e98c101dc91311e92d54be8
Louie Shin <unicus.fortis@gmail.com>
2024-02-14 18:31:33 +0900

    [Update] Typography - 각각 폰트를 리턴하는 함수 생성

--- SeSACTalk/Constant/Typography.swift
+++ SeSACTalk/Constant/Typography.swift
@@ -17,4 +17,34 @@ enum Typography {
     static let caption = UIFont(name: "SFPro-Regular", size: 12)
     static let caption2 = UIFont(name: "SFPro-Regular", size: 11)
     
+    
+    static func createTitle1() -> UIFont {
+        guard let font = Self.title1 else { return UIFont.systemFont(ofSize: 22) }
+        return font
+    }
+    
+    static func createTitle2() -> UIFont {
+        guard let font = Self.title2 else { return UIFont.systemFont(ofSize: 14) }
+        return font
+    }
+    
+    static func createBodyBold() -> UIFont {
+        guard let font = Self.bodyBold else { return UIFont.systemFont(ofSize: 13) }
+        return font
+    }
+    
+    static func createBody() -> UIFont {
+        guard let font = Self.body else { return UIFont.systemFont(ofSize: 13) }
+        return font
+    }
+    
+    static func createCaption() -> UIFont {
+        guard let font = Self.caption else { return UIFont.systemFont(ofSize: 12) }
+        return font
+    }
+    
+    static func createCaption2() -> UIFont {
+        guard let font = Self.caption2 else { return UIFont.systemFont(ofSize: 11) }
+        return font
+    }
 }
