ccc9b8390c00da3ab379be3de7d1932a6ee903f6
Louie Shin <unicus.fortis@gmail.com>
2024-02-15 21:47:57 +0900

    [Add] IDwithWorkspaceIDRequest 생성
    채널 멤버 조회 라우터에 접근시 필요한 구조체 생성

---
+++ SeSACTalk/Model/Request/IDwithWorkspaceIDRequest.swift
@@ -0,0 +1,16 @@
+//
+//  IDwithWorkspaceIDRequest.swift
+//  SeSACTalk
+//
+//  Created by LOUIE MAC on 2/14/24.
+//
+
+import Foundation
+
+
+struct IDwithWorkspaceIDRequest {
+    
+    let name: String
+    let id: Int
+    
+}
