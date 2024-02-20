d4a98f1a8f412f528b6f4813f2a01970f6751194
Louie Shin <unicus.fortis@gmail.com>
2024-02-21 05:48:22 +0900

    [Add] ChannelMemberFeatureClass
    채널 멤버 리스트를 표시할수 있는 Feature Class 생성 - ListingViewControllerProtocol

---
+++ SeSACTalk/Features/ListPresetable/ChannelMemberFeatureClass.swift
@@ -0,0 +1,50 @@
+//
+//  ChannelMemberFeatureClass.swift
+//  SeSACTalk
+//
+//  Created by LOUIE MAC on 2/21/24.
+//
+
+import UIKit
+import RxSwift
+
+final class ChannelMemberFeatureClass: ListingViewControllerProtocol {
+        
+    var session: LoginSession
+    var tableView: UITableView
+    var viewModel: ChannelSettingViewModel
+    var disposeBag = DisposeBag()
+    
+    init(viewModel: ChannelSettingViewModel) {
+        self.session = LoginSession.shared
+        self.tableView = UITableView()
+        self.viewModel = viewModel
+        
+    }
+    
+    func bind(target: UIViewController) {
+        viewModel.userModelsRelay
+            .bind(to: tableView.rx.items(cellIdentifier: MemberListCell.identifier,
+                                         cellType: MemberListCell.self)) {
+                _, item, cell in
+                cell.channelUserData.onNext(item)
+            }.disposed(by: disposeBag)
+    }
+    
+    func setNavigationController(target: UIViewController) {
+        target.navigationController?.setCloseableNavigation(title: "채널 관리자 변경",
+                                                            target: target,
+                                                            action: #selector(target.dismissTrigger))
+    }
+    
+    func registerTableViewCell() {
+        tableView.register(MemberListCell.self,
+                           forCellReuseIdentifier: MemberListCell.identifier)
+    }
+    
+    func setTableViewRowheight() {
+        tableView.rowHeight = 60
+    }
+    
+    func showActionSheet(target: UIViewController, workspace: WorkSpace) { }
+}
