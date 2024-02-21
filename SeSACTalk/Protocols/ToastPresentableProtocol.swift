dba4d1f7292bc9e371621078b175f5c71c863f5e
Louie Shin <unicus.fortis@gmail.com>
2024-02-21 21:20:55 +0900

    [Update] ToastPresentableProtocol - View 바텀 세이프 에리어에 등장하는 메세지 노출 메서드 생성

--- SeSACTalk/Protocols/ToastPresentableProtocol.swift
+++ SeSACTalk/Protocols/ToastPresentableProtocol.swift
@@ -42,5 +42,28 @@ extension ToastPresentableProtocol where Self: UIViewController {
             overlayView.removeFromSuperview()
         }
     }
+    
+    func makeSuccessToastView(message: String, backgroundColor: UIColor, target: UIViewController) {
+        let overlayView = UIView(frame: CGRect(x: 0,
+                                               y: 0,
+                                               width: 200,
+                                               height: 50))
+        overlayView.backgroundColor = UIColor.clear
+        self.view.addSubview(overlayView)
+        overlayView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(target.view.safeAreaLayoutGuide)
+            make.bottom.equalTo(target.view.safeAreaLayoutGuide)
+        }
+        
+        var style = ToastStyle()
+        style.backgroundColor = backgroundColor
+        style.messageNumberOfLines = 0
+        overlayView.makeToast(message, duration: 2.0, position: .center, style: style)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            overlayView.removeFromSuperview()
+        }
+    }
 }
 
