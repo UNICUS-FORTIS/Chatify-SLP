c0626e1d3faab249ab44c179a8b544e3ac5dc71f
Louie Shin <unicus.fortis@gmail.com>
2024-02-26 23:59:43 +0900

    [Update] SocialLoginHandler
    카카오의 UserApi 의 응답과 앱 서버의 social 로그인에 대한 액션을 SocialLoginHandler 로 분리

--- SeSACTalk/Helper/SocialLoginHandler.swift
+++ SeSACTalk/Helper/SocialLoginHandler.swift
@@ -7,15 +7,47 @@
 
 import Foundation
 import RxSwift
+import KakaoSDKCommon
+import RxKakaoSDKCommon
+import KakaoSDKAuth
+import RxKakaoSDKAuth
+import KakaoSDKUser
+import RxKakaoSDKUser
+import AuthenticationServices
 
 
-struct SocialLoginHandler {
+final class SocialLoginHandler {
     
     private let networkService = NetworkService.shared
+    private let session = LoginSession.shared
+    private let disposeBag = DisposeBag()
     
-    func fetchKakaoLoginRequest(info: KakaoLoginRequest) -> Single<Result<SignInResponse, ErrorResponse>> {
-        return networkService.fetchRequest(endpoint: .kakaoLogin(model: info),
-                                           decodeModel: SignInResponse.self)
+    func fetchKakaoLoginRequest(completion: @escaping () -> Void ) {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.rx.loginWithKakaoTalk()
+                .flatMap { token -> Observable<KakaoLoginRequest> in
+                    guard let deviceToken = UserdefaultManager.createDeviceToken() else { return Observable.empty() }
+                    let form = KakaoLoginRequest(oauthToken: token.accessToken,
+                                                 deviceToken: deviceToken)
+                    return Observable.just(form)
+                }
+                .flatMap { request in
+                    return self.networkService.fetchRequest(endpoint: .kakaoLogin(model: request),
+                                                            decodeModel: SignInResponse.self)
+                }.subscribe(with: self) { owner, result in
+                    switch result {
+                    case .success(let response):
+                        owner.session.handOverLoginInformation(userID: response.userID,
+                                                         nick: response.nickname,
+                                                         access: response.token.accessToken,
+                                                         refresh: response.token.refreshToken)
+                        completion()
+                    case .failure(let error):
+                        print(error.errorCode)
+                    }
+                }
+                .disposed(by: disposeBag)
+        }
     }
     
     func fetchAppleLoginRequest(info: AppleLoginRequest) -> Single<Result<SignInResponse, ErrorResponse>> {
@@ -23,4 +55,5 @@ struct SocialLoginHandler {
         return networkService.fetchRequest(endpoint: .appleLogin(model: info),
                                            decodeModel: SignInResponse.self)
     }
+    
 }
