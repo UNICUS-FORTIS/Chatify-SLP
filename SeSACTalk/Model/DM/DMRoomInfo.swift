f011a26b3bee589ea7113392b9589feebf35f864
Louie Shin <unicus.fortis@gmail.com>
2024-02-28 23:21:33 +0900

    [Add] DMRoomInfo - DM 소켓매니저에서 DM데이터 조회시 받아오는 DM 룸 정보를 할당 가능한 Struct 생성

---
+++ SeSACTalk/Model/DM/DMRoomInfo.swift
@@ -0,0 +1,15 @@
+//
+//  DMRoomInfo.swift
+//  SeSACTalk
+//
+//  Created by LOUIE MAC on 2/28/24.
+//
+
+import Foundation
+
+struct DMRoomInfo {
+    
+    let roomID: Int
+    let workspaceID: Int
+    
+}
