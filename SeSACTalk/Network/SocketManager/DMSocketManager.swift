c5bda51eecaf2894bcf5a548d7e22ad4bb15b5c7
Louie Shin <unicus.fortis@gmail.com>
2024-02-29 07:38:09 +0900

    [Feat] DM 채팅 기능

---
+++ SeSACTalk/Network/SocketManager/DMSocketManager.swift
@@ -0,0 +1,184 @@
+//
+//  DMSocketManager.swift
+//  SeSACTalk
+//
+//  Created by LOUIE MAC on 2/27/24.
+//
+
+
+import Foundation
+import SocketIO
+import RxSwift
+import RxCocoa
+
+
+final class DMSocketManager: NSObject {
+    
+    private var manager: SocketManager?
+    private var socket: SocketIOClient?
+    
+    var channelInfo: Channels?
+    
+    private let networkService = NetworkService.shared
+    private let repository = DMRealmRepository.shared
+    private var task: Results<DM>?
+    private let disposeBag = DisposeBag()
+    
+    var channelChatRelay = BehaviorRelay<[ChannelDataSource]>(value: [])
+    var dmChatRelay = BehaviorRelay<[DMDataSource]>(value: [])
+    var dmRoomInfo: DMRoomInfo?
+    var temporaryChats = [DMDataSource]()
+    var cursorDate: String?
+    var scroller: ( ()  -> Void )?
+    
+    init(dmInfo: DMs) {
+        super.init()
+        self.dmRoomInfo = DMRoomInfo(roomID: dmInfo.roomID, workspaceID: dmInfo.workspaceID)
+        startDM(targetUserID: dmInfo.user.userID, workspaceID: dmInfo.workspaceID)
+    }
+    
+    init(targetUserID: Int, workspaceID: Int) {
+        super.init()
+        startDM(targetUserID: targetUserID, workspaceID: workspaceID)
+    }
+    
+    func startDM(targetUserID: Int, workspaceID: Int) {
+        let workspace = IDRequiredRequest(id: workspaceID)
+        let target = IDRequiredRequest(id: targetUserID)
+        let cursor = ChatCursorDateRequest(cursor: self.getCursorDate(workspaceID: workspaceID,
+                                                                      withUserID: targetUserID))
+        print(cursor,"커서다---------@@@@@@@")
+        networkService.fetchRequest(endpoint: .loadDMChats(workspaceID: workspace,
+                                                           targetUserID: target,
+                                                           cursor: cursor), decodeModel: DMChatResponse.self)
+        .subscribe(with: self) { owner, result in
+            switch result {
+            case .success(let response):
+                print(response)
+                DispatchQueue.main.async {
+                    self.dmRoomInfo = DMRoomInfo(roomID: response.roomID, workspaceID: response.workspaceID)
+                    owner.task = owner.repository.fetchDMDatas(dmRoomID: response.roomID,
+                                                               workspaceID: response.workspaceID)
+                    owner.loadRecentChatFromDatabse()
+                    if !response.chats.isEmpty {
+                        owner.repository.updateUnreadDM(dm: response)
+                        let newDatas = response.chats.map { DMDataSource(dm: $0) }
+                        var currentData = owner.dmChatRelay.value
+                        currentData.append(contentsOf: newDatas)
+                        owner.dmChatRelay.accept(currentData)
+                        owner.scroller?()
+                    }
+                    owner.scroller?()
+                }
+                
+                guard let url = URL(string: EndPoints.baseURL +
+                                    EndPoints.Paths.PathDepthOne.dmSocket +
+                                    "\(response.roomID)") else { return }
+                
+                owner.manager = SocketManager(socketURL: url,
+                                              config: [.compress,
+                                                       .forceWebsockets(true)])
+                
+                owner.socket = owner.manager?.socket(forNamespace: "/ws-dm-" + "\(response.roomID)")
+                
+                owner.socket?.on(clientEvent: .ping) { data, ack in
+                    print("ping")
+                }
+                
+                owner.socket?.on(clientEvent: .disconnect) { data, ack in
+                    print("SOCKET IS DISCONNECTED", data, ack)
+                }
+                
+                owner.socket?.on("dm") { dataArray, ack in
+                    print("🏷️🏷️🏷️ dm")
+                    guard let received = dataArray.first as? [String: Any],
+                          let roomInfo = self.dmRoomInfo else { return }
+                    do {
+                        let serialized = try JSONSerialization.data(withJSONObject:received)
+                        let decoded = try JSONDecoder().decode(DMChatModel.self,from: serialized)
+                        print(decoded)
+                        var messages = self.dmChatRelay.value
+                        let dmDatasource = DMDataSource(dm: decoded)
+                        messages.append(dmDatasource)
+                        self.dmChatRelay.accept(messages)
+                        self.repository.updateDM(workspaceID: roomInfo.workspaceID,
+                                                 dm: decoded)
+                    }
+                    catch {
+                        print(error)
+                    }
+                    
+                    owner.connectSocket()
+                }
+                
+            case .failure(let error):
+                print(error.errorCode)
+            }
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    deinit {
+        print("소켓매니저 Deinit됨")
+    }
+}
+
+extension DMSocketManager: ChatProtocol {
+    
+    func messageSender<T>(request: T) {
+        guard let casted = request as? ChatBodyRequest,
+              let safeDMInfo = dmRoomInfo else { return }
+        let workspaceIDRequest = IDRequiredRequest(id: safeDMInfo.workspaceID)
+        let dmRoomIDRequest = IDRequiredRequest(id: safeDMInfo.roomID)
+        networkService.fetchStatusCodeRequest(endpoint: .sendDMsChat(workspaceID: workspaceIDRequest,
+                                                                     roomID: dmRoomIDRequest,
+                                                                     contents: casted))
+        .subscribe(with: self) { owner, result in
+            switch result {
+            case .success(let statuscode) :
+                print(statuscode)
+                
+            case .failure(let error) :
+                print(error.errorCode)
+            }
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    func loadChatLog(completion: @escaping () -> Void) {
+        guard let safeTask = self.task else { return }
+        completion()
+    }
+    
+    func getCursorDate(workspaceID: Int, withUserID: Int) -> String {
+        return repository.getDMLatestChatData(workspaceID: workspaceID,
+                                              userID: withUserID) ?? getCurrentTimeForCursor()
+    }
+    
+    func getCursorDate(channelInfo: Channels) -> String { return "" }
+    
+    func checkRelayIsEmpty() -> Bool {
+        return dmChatRelay.value.isEmpty
+    }
+    
+    func loadRecentChatFromDatabse() {
+        print(#function)
+        guard let safeTask = task,
+              let target = safeTask.first?.dmDataArray else { return }
+        if target.count > 50 {
+            let arrayConverted = Array(target.suffix(50))
+            dmChatRelay.accept(arrayConverted)
+        } else {
+            dmChatRelay.accept(target)
+        }
+    }
+    
+    func connectSocket() {
+        socket?.connect()
+    }
+    
+    func disconnectSocket() {
+        socket?.removeAllHandlers()
+        socket?.disconnect()
+    }
+}
---
+++ SeSACTalk/View/Chat/DMChatViewController.swift
@@ -0,0 +1,183 @@
+//
+//  DMChatViewController.swift
+//  SeSACTalk
+//
+//  Created by LOUIE MAC on 2/27/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+final class DMChatViewController: UIViewController {
+    
+    var socketManager: ChatProtocol
+    var session: LoginSession
+    var tableView: UITableView
+    var accView: CustomInputAccView
+    var disposeBag: DisposeBag
+    
+    var lastIndexPath: IndexPath? {
+        guard let lastRow = tableView.numberOfRows(inSection: 0) > 1 ?
+                tableView.numberOfRows(inSection: 0) :
+                    nil else { return nil }
+        return IndexPath(row: lastRow, section: 0)
+    }
+    
+    init(manager: ChatProtocol) {
+        self.socketManager = manager
+        self.session = LoginSession.shared
+        self.tableView = UITableView()
+        self.accView = CustomInputAccView()
+        self.disposeBag = DisposeBag()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        setConstraints()
+        setTextViewDelegate()
+        setupNavigation()
+        setupSendButton()
+        setupTapGesture()
+        bindTableView()
+        bindTableViewScrollable()
+        setTextViewDelegate()
+        scrollToNewestChat()
+        keyboardSetting(target: self,
+                        view: accView,
+                        tableView: tableView,
+                        scrollView: nil,
+                        disposeBag: disposeBag)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        socketManager.connectSocket()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        socketManager.disconnectSocket()
+    }
+    
+    @objc private func setNavigationRightAction() {
+        guard let channelInfo = socketManager.channelInfo else { return }
+        let vc = ChannelSettingViewController(channelInfo: channelInfo)
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    deinit {
+        print("DM 챗 뷰컨트롤러 deinit 됨")
+    }
+}
+
+
+extension DMChatViewController: ChatInterfaceAcceptableProtocol {
+   
+
+    func setTextViewDelegate() {
+        accView.textView.delegate = self
+    }
+    
+    func setupTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tableView.addGestureRecognizer(tapGesture)
+    }
+    
+    func setupNavigation() {
+        navigationController?.setChannelChatNavigation(target: self,
+                                                       title: socketManager.channelInfo?.name ?? "",
+                                                       rightAction: #selector(setNavigationRightAction))
+    }
+    
+    func bindTableView() {
+        socketManager.dmChatRelay
+            .map  { chats in
+                chats.sorted { $0.createdAt < $1.createdAt }
+            }
+            .bind(to: tableView.rx.items) { [weak self] (tableView, row, item) -> UITableViewCell in
+                if self?.socketManager.judgeSender(sender: self?.session.makeUserID() ?? 00,
+                                                   userID: item.user?.userID ?? 00) ?? true {
+                    
+                    guard let senderCell = tableView.dequeueReusableCell(withIdentifier: MessageSenderTableCell.identifier) as? MessageSenderTableCell else { return UITableViewCell() }
+                    
+                    senderCell.bind(dmData: item)
+                    return senderCell
+                    
+                } else {
+                    
+                    guard let otherUserCell = tableView.dequeueReusableCell(withIdentifier: MessageTableCell.identifier) as? MessageTableCell else { return UITableViewCell() }
+                    
+                    otherUserCell.bind(dmData: item)
+                    return otherUserCell
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func setupSendButton() {
+        accView.rightButton.rx.tap
+            .bind(with: self) { owner, _ in
+                let message = owner.accView.textView.text ?? ""
+                let files: [Data] = []
+                let request = ChatBodyRequest(content: message, files: files)
+                owner.socketManager.messageSender(request: request)
+                owner.accView.textView.text = ""
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func scrollToNewestChat() {
+        socketManager.scroller = { [weak self] in
+            if self?.socketManager.checkRelayIsEmpty() == false {
+                self?.scrollToEnd()
+            }
+        }
+    }
+    
+    func scrollToEnd() {
+        let count = socketManager.dmChatRelay.value.count
+        if count > 1 {
+            let last = count - 1
+            DispatchQueue.main.async {
+                let indexPath = IndexPath(row: last, section: 0)
+                self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: false)
+            }
+        }
+    }
+    
+    func bindTableViewScrollable() {
+        socketManager.dmChatRelay
+            .bind(with: self) { owner, _ in
+                owner.scrollToEnd()
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+extension DMChatViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.textColor = .black
+        textView.text = nil
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "메세지를 입력하세요"
+            textView.textColor = UIColor.lightGray
+            setInitialIconColor()
+        }
+    }
+}
