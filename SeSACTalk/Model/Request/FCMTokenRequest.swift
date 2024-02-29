87528db4201dc199b30934e72118103092840053
Louie Shin <unicus.fortis@gmail.com>
2024-02-29 23:45:43 +0900

    [Add] FCMTokenRequest - FCM 디바이스 토큰 Encodable struct 생성

---
+++ SeSACTalk/Model/Request/FCMTokenRequest.swift
@@ -0,0 +1,15 @@
+//
+//  FCMTokenRequest.swift
+//  SeSACTalk
+//
+//  Created by LOUIE MAC on 2/29/24.
+//
+
+import Foundation
+
+
+struct FCMTokenRequest:Encodable {
+    
+    let deviceToken: String
+    
+}
