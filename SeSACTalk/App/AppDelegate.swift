2bef58318da05a0656c061d1d6b7ec05e180a6bb
Louie Shin <unicus.fortis@gmail.com>
2024-03-01 23:49:50 +0900

    [Update] AppDelegate - Firebase FCM 토큰 저장 및 전송 메서드 생성

--- SeSACTalk/App/AppDelegate.swift
+++ SeSACTalk/App/AppDelegate.swift
@@ -9,35 +9,47 @@ import UIKit
 import Kingfisher
 import KakaoSDKCommon
 import RxKakaoSDKCommon
-
+import UserNotifications
+import FirebaseCore
+import FirebaseMessaging
+import iamport_ios
 
 @main
- class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
     
-     var modifier = AnyModifier { request in
-         var newRequest = request
-         newRequest.setValue(SecureKeys.Headers.accessToken,
-                             forHTTPHeaderField: SecureKeys.Headers.auth)
-         newRequest.setValue(SecureKeys.APIKey.secretKey,
-                             forHTTPHeaderField: SecureKeys.Headers.Headerkey)
-         return newRequest
-     }
+    var modifier = AnyModifier { request in
+        var newRequest = request
+        newRequest.setValue(SecureKeys.Headers.accessToken,
+                            forHTTPHeaderField: SecureKeys.Headers.auth)
+        newRequest.setValue(SecureKeys.APIKey.secretKey,
+                            forHTTPHeaderField: SecureKeys.Headers.Headerkey)
+        return newRequest
+    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-                
-        UNUserNotificationCenter.current().delegate = self
-
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(
-          options: authOptions,
-          completionHandler: { _, _ in }
-        )
-
+        
         application.registerForRemoteNotifications()
         RxKakaoSDK.initSDK(appKey: SecureKeys.NativeAppKey.KakaoNativeAppKey)
         KingfisherManager.shared.defaultOptions = [.requestModifier(modifier)]
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        sleep(2)
+        return true
+    }
 
-      
+    // MARK: - portone setting
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        Iamport.shared.receivedURL(url)
         return true
     }
     
@@ -56,17 +68,28 @@ import RxKakaoSDKCommon
     }
     
     
-
+    
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
-
+    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
         UserdefaultManager.saveDeviceToken(token: token)
-        print("디바이스토큰", token)
-
+        print(token)
     }
     
 }
 
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        let session = LoginSession.shared
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        print(fcmToken, "FCM 토큰")
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        guard let token = fcmToken else { return }
+        UserdefaultManager.saveFCMToken(token: token)
+        session.fetchSaveFCMToken(token: token)
+    }
+}
